236578e820e40d0300a282108a9e5d776557e940
Mattia-Carbonera <mattia.carbonera@gmail.com>
2025-03-25 01:50:57 +0100

    add image on homePage

--- src/main/java/org/lessons/pizzas/java/spring_la_mia_pizzeria_crud/controllers/pizzasController.java
+++ src/main/java/org/lessons/pizzas/java/spring_la_mia_pizzeria_crud/controllers/pizzasController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 
